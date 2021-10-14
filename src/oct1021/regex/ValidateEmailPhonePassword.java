633ab7fff950d9037840ca295f07782bbce9568e
sumeetyajnik <70086010+sumeetyajnik@users.noreply.github.com>
2021-10-14 19:09:07 +0530

    new changes

--- src/oct1021/regex/ValidateEmailPhonePassword.java
+++ src/oct1021/regex/ValidateEmailPhonePassword.java
@@ -2,7 +2,7 @@ package oct1021.regex;
 
 import java.util.regex.Pattern;
 
-public class ValidateEmailPhone {
+public class ValidateEmailPhonePassword {
 
 	public static void main(String[] args) {
 		 boolean email1 = isEmailValid("firstname@gmail.com");
@@ -14,10 +14,19 @@ public class ValidateEmailPhone {
 		 
 		 boolean phone1 = isPhoneValid("9873333333");
 		 System.out.println("phone1 : " + phone1);
-		 boolean phone2 = isPhoneValid("test123prepare@co.edu.com");
+		 boolean phone2 = isPhoneValid("+919873333333");
 		 System.out.println("phone2 : " + phone2);
-		 boolean phone3 = isPhoneValid("test123prepare@co.edu.com");
+		 boolean phone3 = isPhoneValid("+9188888");
 		 System.out.println("phone3 : " + phone3);
+		 boolean phone4 = isPhoneValid("+918888888888888");
+		 System.out.println("phone3 : " + phone4);
+		 boolean phone5 = isPhoneValid("98733333331");
+		 System.out.println("phone5 : " + phone5);
+		 boolean phone6 = isPhoneValid("987333333");
+		 System.out.println("phone6 : " + phone6);
+		 
+		 boolean password1 = isPasswordValid("Ar1Tt4l8@p");
+		 System.out.println("password1 : " + password1);
 	}
 	
 	public static boolean isEmailValid(String email) {
@@ -26,10 +35,16 @@ public class ValidateEmailPhone {
 	}
 	
 	public static boolean isPhoneValid(String phone) {
-		 final Pattern PHONE_REGEX = Pattern.compile("^(?:(?:\\+|0{0,2})91(\\s*[\\ -]\\s*)?|[0]?)?[789]\\d{9}|(\\d[ -]?){10}\\d$", Pattern.CASE_INSENSITIVE);
+		 final Pattern PHONE_REGEX = Pattern.compile("^((\\+91)?)[789]{1}\\d{9}$", Pattern.CASE_INSENSITIVE);
 		 return PHONE_REGEX.matcher(phone).matches();
 	}
 	
+	
+	public static boolean isPasswordValid(String password) {	
+	       String regex = "(?=[A-Z])(?=(.*[\\d]){3})(?=.*[a-z])(?=.*[@#$%^&+=])(?=\\S+$).{10,10}$";
+		 final Pattern PASSWORD_REGEX = Pattern.compile(regex);
+		 return PASSWORD_REGEX.matcher(password).matches();
+	}
 	
 	
 
---
+++ src/oct1021/simpleWaitNotify/SimpleWaitNotify.java
@@ -0,0 +1,60 @@
+package oct1021.simpleWaitNotify;
+
+class Customer {
+	int amount = 10000;
+
+	synchronized void withdraw(int amount) {
+		System.out.println("going to withdraw..." +amount);
+
+		if (this.amount < amount) {
+			System.out.println("Less balance; waiting for deposit...");
+			try {
+				wait();
+			} catch (Exception e) {
+			}
+		}
+		this.amount -= amount;
+		System.out.println("withdraw completed...the left over amount is " + this.amount);
+	}
+
+	synchronized void deposit(int amount) {
+		System.out.println("going to deposit..." +amount) ;
+		this.amount += amount;
+
+		System.out.println("deposit completed... " + this.amount);
+		notify();
+	}
+}
+
+public class SimpleWaitNotify {
+	public static void main(String args[]) {
+		final Customer c = new Customer();
+		
+		new Thread() {
+			public void run() {
+				c.withdraw(4000);
+			}
+		}.start();
+		
+		new Thread() {
+			public void run() {
+				c.withdraw(5000);
+			}
+		}.start();
+	
+		
+		new Thread() {
+			public void run() {
+				c.deposit(20000);
+			}
+		}.start();
+
+		new Thread() {
+			public void run() {
+				c.withdraw(10000);
+			}
+		}.start();
+
+
+	}
+}
